4627f030c44941fcd4104168ab3e451511381e5d
unknown <you@example.com>
2024-04-24 11:09:56 -0300

    versão principal

---
+++ data/index.js
@@ -0,0 +1,47 @@
+let DATA = [
+{
+    id : '01',
+    porcentagem : '5%',
+    titulo : 'leitura',
+    imagem : 'https://gramaticaonline.com.br/wp-content/uploads/2018/11/Tipos-de-leitura.jpg',
+    descricao : 'A alfabetização é uma fase primordial no desenvolvimento das crianças. Para isso, é preciso ser o mais didático possível no processo de aprendizagem de leitura e interpretação dos pequenos.',
+},
+{
+    id : '02',
+    porcentagem : '10%',
+    titulo : 'Videogame',
+    imagem : 'https://i0.wp.com/jornalcomunicacao.ufpr.br/wp-content/uploads/2023/10/jornalcomunicacao.ufpr.br-strongo-impacto-dos-videogames-na-sociedadestrong-foto-ensaio-videogame-scaled.jpg?fit=2560%2C1678&ssl=1',
+    descricao : 'A alfabetização é uma fase primordial no desenvolvimento das crianças. Para isso, é preciso ser o mais didático possível no processo de aprendizagem de leitura e interpretação dos pequenos.',
+},
+{
+    id : '03',
+    porcentagem : '15%',
+    titulo : 'Alimentação',
+    imagem : 'https://neofeed.com.br/wp-content/uploads/2023/06/alimentos-mundo.jpg',
+    descricao : 'A alfabetização é uma fase primordial no desenvolvimento das crianças. Para isso, é preciso ser o mais didático possível no processo de aprendizagem de leitura e interpretação dos pequenos.',
+},
+{
+    id : '04',
+    porcentagem : '20%',
+    titulo : 'Judô',
+    imagem : 'https://www.institutoclaro.org.br/educacao/wp-content/uploads/sites/2/2022/11/judo-destaque.png',
+    descricao : 'A alfabetização é uma fase primordial no desenvolvimento das crianças. Para isso, é preciso ser o mais didático possível no processo de aprendizagem de leitura e interpretação dos pequenos.',
+},
+{
+    id : '05',
+    porcentagem : '25%',
+    titulo : 'Cuidados pessoais',
+    imagem : 'https://blog.keydesign.com.br/wp-content/uploads/2018/05/beleza-masculina-em-alta.jpg',
+    descricao : 'A alfabetização é uma fase primordial no desenvolvimento das crianças. Para isso, é preciso ser o mais didático possível no processo de aprendizagem de leitura e interpretação dos pequenos.',
+},
+{
+    id : '06',
+    porcentagem : '30%',
+    titulo : 'Surtos Coletivos',
+    imagem : 'https://www.psiquiatraportoalegre.com.br/wp-content/uploads/2021/07/depressao-mais-incapacitante-doenca-psiquiatra-portoalegre-ciulla.jpg',
+    descricao : 'A alfabetização é uma fase primordial no desenvolvimento das crianças. Para isso, é preciso ser o mais didático possível no processo de aprendizagem de leitura e interpretação dos pequenos.',
+},
+
+]
+
+export default DATA;
